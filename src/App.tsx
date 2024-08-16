488d5f214e4546b746c0f98289912fd35e953e23
dngvbngc <dvbngoc.0706@gmail.com>
2024-08-16 11:22:30 +0800

    error catching in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import { extractTimestampsAndLyrics } from "./utils.ts/videoUtils";
 import transcripts from "../data/transcripts";
 import { useState } from "react";
-import { Button, Heading } from "@chakra-ui/react";
+import { Button, Heading, Text } from "@chakra-ui/react";
 import VideoPlayer from "./components/videoPlayer";
 
 const App = () => {
   const transcript = transcripts[0];
   const videoPortions = extractTimestampsAndLyrics(transcript.content);
   const [currentLine, setCurrentLine] = useState(0);
+
+  if (currentLine == videoPortions.length)
+    return (
+      <>
+        <Heading>
+          {transcript.name} - {transcript.artist}
+        </Heading>
+        <Text>Congratulations! You have learned this song!</Text>
+        <Button onClick={() => setCurrentLine(0)}>Restart</Button>
+      </>
+    );
+
   const startTime =
     videoPortions.length > 0 ? videoPortions[currentLine].timestamp : 0;
   const endTime =
     currentLine < videoPortions.length - 1
       ? videoPortions[currentLine + 1].timestamp
-      : 0;
+      : transcript.duration;
 
   return (
     <>
@@ -21,6 +33,7 @@ const App = () => {
         {transcript.name} - {transcript.artist}
       </Heading>
       <VideoPlayer startTime={startTime} endTime={endTime}></VideoPlayer>
+      <Text>{transcript.content[currentLine].lyrics}</Text>
       <Button onClick={() => setCurrentLine(currentLine + 1)}>Click</Button>
     </>
   );
